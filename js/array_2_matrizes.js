ce6e25db0383566dae5b3bd30bcc3a715b813d10
devcharle <charlexavier100@gmail.com>
2022-09-11 19:31:33 -0300

    enviando repositorio local para remoto

---
+++ js/array_2_matrizes.js
@@ -0,0 +1,8 @@
+
+
+var  matriz =[['2','0','0'],['2','1','2'],['2','2','4'],['2','3','6'],
+['2','4','8'],['2','5','10'],['2','6','12'],['2','7','14'],['2','8','16'],
+['2','9','18'],['2','10','20']];
+alert('dentro da matemarica é uma representação de álgebra linear')
+alert('Álgebra Linear é um ramo da matemática que lida com equações lineares e funções lineares que são representadas através de matrizes e vetores')
+console.log(matriz);
